493a178f66d6bc90de915fbf596f528ebc9d73af
Paul Young <evoke@youngish.org>
2022-03-11 17:19:35 +1030

    Add contact form handling.

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,3 +1,5 @@
+import {PublishCommand} from "@aws-sdk/client-sns"
+import {snsClient} from "../aws/sns-client"
 import React, {FormEventHandler, useState} from 'react'
 
 import './contact-form.scss'
@@ -5,6 +7,27 @@ import './contact-form.scss'
 export const ContactForm = (): JSX.Element => {
     const [message, setMessage] = useState("");
     const submitContact: FormEventHandler = e => {
+
+        (
+            async () => {
+                try {
+                    const data = await snsClient.send(
+                        new PublishCommand({
+                            TopicArn: "arn:aws:sns:ap-southeast-2:354785679550:youngish-org-contact-form",
+                            Message: message,
+                            Subject: "youngish.org contact form"
+                        })
+                    )
+
+                    console.log("Success.", data)
+
+                    return data;
+                } catch (err : unknown) {
+                    console.log("Error", err)
+                }
+            }
+        )();
+
         alert(message);
     };
 
