bd4c43462e7f0f2cfb2e719aaddbff59994ba826
Paul Young <evoke@youngish.org>
2022-03-14 09:53:44 +1030

    Add resume.

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -5,6 +5,6 @@ import "./status.scss"
 export const Status = (): JSX.Element => (
     <section className="status">
         <h1>Status</h1>
-        <p>I am currently looking for full-time work.</p>
+        <p>I am currently looking for full-time work.  Please see my <a href="/docs/resume.pdf">resume</a>.</p>
     </section>
 )
